884e4d04ba699a625cdd4a7aeeef41f0a2124e1d
entechlog <38563714+entechlog@users.noreply.github.com>
2020-10-25 01:49:29 -0400

    Made changes to use kafka avro producer

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ docker build --tag entechlog/weather-alert-app .
 
 docker images
 
-docker run -it --rm  -e bootstrap_servers=192.168.1.8:39092 -e topic_name=weather.alert.app.source -e input_location=Marthandam,IN -e OPEN_WEATHER_API_KEY=secure entechlog/weather-alert-app
+docker run -it --rm  -e bootstrap_servers=192.168.1.8:39092 -e 
+schema_registry_url=http://192.168.1.8:8081 -e topic_name=weather.alert.app.source -e lat=38.88 -e lon=-94.82 -e OPEN_WEATHER_API_KEY=secure entechlog/weather-alert-app
 ```
--- zTemp/weather-alert-app-original.py
+++ zTemp/weather-alert-app-original.py
@@ -3,29 +3,11 @@
 # importing the required library 
 from confluent_kafka import Producer
 
-### Adding for Avro producer support
-from confluent_kafka import avro
-from confluent_kafka.avro import AvroProducer
-from confluent_kafka.avro.serializer import (KeySerializerError, ValueSerializerError)
-
-from confluent_kafka import SerializingProducer
-from confluent_kafka.serialization import StringSerializer
-from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.avro import AvroSerializer
-
-import confluent_kafka
 import requests
 import os, sys, time
 import argparse
 import json
 import socket
-
-import schemas
-
-"""
-https://github.com/confluentinc/examples/blob/6.0.0-post/clients/cloud/python/producer_ccsr.py
-https://github.com/filipovskid/DRBoson/
-"""
 
 def header_footer(process):
     # Display the program start time
@@ -46,7 +28,6 @@ def parse_input():
 
     parser.add_argument("--bootstrap_servers", help="Bootstrap servers", required=True)
     parser.add_argument("--topic_name", help="Topic name", required=True)
-    parser.add_argument("--schema_registry_url", help="Schema registry url", required=True)
     parser.add_argument("--lat", help="latitude", required=True)
     parser.add_argument("--lon", help="longitude", required=True)
     
@@ -54,17 +35,15 @@ def parse_input():
 
     bootstrap_servers = args.bootstrap_servers
     topic_name = args.topic_name
-    schema_registry_url = args.schema_registry_url
     lat = args.lat
     lon = args.lon
 
-    print("Bootstrap servers            : ", bootstrap_servers)
-    print("Topic name                   : ", topic_name)
-    print("Schema registry url          : ", schema_registry_url)
-    print("Latitude                     : ", lat)
-    print("Longitude                    : ", lon)
+    print("Bootstrap servers            : " + bootstrap_servers)
+    print("Topic name                   : " + topic_name)
+    print("Latitude                     : " + lat)
+    print("Longitude                    : " + lon)
 
-    return bootstrap_servers, schema_registry_url, topic_name, lat, lon
+    return bootstrap_servers, topic_name, lat, lon
 
 def call_weather_api(URL, PARAMS):    
     # sending get request and saving the response as response object 
@@ -104,49 +83,23 @@ def process_data(json_data):
     weather = data['current']['weather'][0]['description'] 
     temperature = data['current']['temp']
     
-    # Print the output 
-    print("Timestamp                    : ", str(timestamp))
-    print("Latitude                     : ", str(latitude))
-    print("Longitude                    : ", str(longitude))
-    print("Weather Description          : ", weather)
-    print("Temperature                  : ", str(temperature))
-
-def delivery_report(err, msg):
-    """ Called once for each message produced to indicate delivery result.
-        Triggered by poll() or flush(). """
-    if err is not None:
-        print("Key Type                     : ", type(key))
-        print('Message delivery failed      : {}'.format(err))
-    else:
-        print('Message delivered to         : {} [{}]'.format(msg.topic(), msg.partition()))
-
-def write_to_kafka(bootstrap_servers, schema_registry_url, topic_name, data):
-
-    print("Kafka Version                : ", confluent_kafka.version(),confluent_kafka.libversion())
-
-    schema_registry_conf = {'url': schema_registry_url}
-    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
-
-    value_avro_serializer = AvroSerializer(schemas.weather_source_schema, schema_registry_client)
-    string_serializer = StringSerializer('utf-8')
-
-    conf = {'bootstrap.servers': bootstrap_servers,
-            'client.id': socket.gethostname(),
-            'on_delivery': delivery_report,
-            'key.serializer': string_serializer,
-            'value.serializer': value_avro_serializer    
-            }
-
-    avroProducer = SerializingProducer(conf)
+    # printing the output 
+    print("Timestamp                    : " + str(timestamp))
+    print("Location Name                : " + name)
+    print("Latitude                     : " + str(latitude))
+    print("Longitude                    : " + str(longitude))
+    print("Weather                      : " + weather)
+    print("Temperature                  : " + str(temperature))
     
-    key=str(data['lat']) + '~' + str(data['lon'])
+def write_to_kafka(bootstrap_servers, topic_name, data):
+    conf = {'bootstrap.servers': bootstrap_servers,
+        'client.id': socket.gethostname()}
+        
+    producer = Producer(conf)
     message = json.dumps(data, cls=DatetimeEncoder)
-
-    print("Key Type                     : ", type(key))
-    print("Value Type                   : ", type(json.loads(message)))
-  
-    avroProducer.produce(topic=topic_name, key=key, value=json.loads(message))
-    avroProducer.flush()
+    key=str(data['lat']) + str(data['lon'])
+    producer.produce(topic=topic_name, value=message, key=key)
+    producer.flush()
 
 if __name__ == "__main__":
     
@@ -154,7 +107,7 @@ if __name__ == "__main__":
     header_footer("started")
 
     # Parse the input
-    bootstrap_servers, schema_registry_url, topic_name, lat, lon = parse_input()
+    bootstrap_servers, topic_name, lat, lon = parse_input()
     
     # api-endpoint 
     URL = "https://api.openweathermap.org/data/2.5/onecall"
@@ -173,7 +126,7 @@ if __name__ == "__main__":
 
     # write data to kafka
     if data is not None:
-        write_to_kafka(bootstrap_servers, schema_registry_url, topic_name, data.json())
+        write_to_kafka(bootstrap_servers, topic_name, data.json())
 
     # Print the footer
     header_footer("finished")
